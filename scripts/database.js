fc03d88d03005a4b278383da61e7c05c765ba1a5
himanshup <himanshup@csu.fullerton.edu>
2018-07-18 16:31:19 -0700

    added files to generate book info

---
+++ scripts/database.js
@@ -0,0 +1,76 @@
+(function (window) {
+    "use strict";
+    var App = window.App || {};
+    var $ = window.jQuery;
+
+    function DataBase(url) {
+        if (!url) {
+            throw new Error("No remote URL supplied.");
+        }
+        this.serverUrl = url;
+    }
+
+    DataBase.prototype.initialize = function (numberOfBooks) {
+        var url = this.serverUrl;
+        $.get(url, function (serverResponse) {
+            if (serverResponse.length < numberOfBooks) {
+                var difference = numberOfBooks - serverResponse.length;
+                var template = {
+                    "bookId": 0,
+                    "liked": 0,
+                    "total": 0
+                };
+                for (var i = 0; i < difference; i++) {
+                    template.bookId = i;
+                    $.post(url, template);
+                }
+            }
+        });
+    };
+
+    DataBase.prototype.getAll = function (cb) {
+        $.get(this.serverUrl, function (serverResponse) {
+            cb(serverResponse);
+        });
+    };
+
+    DataBase.prototype.getBookInfo = function (bookId, cb) {
+        $.get(this.serverUrl + "?bookId=" + bookId, function (serverResponse) {
+            cb(serverResponse);
+        });
+    };
+
+    DataBase.prototype.updateLiked = function (bookId) {
+        var url = this.serverUrl;
+        $.get(url + "?bookId=" + bookId, function (serverResponse) {
+            var id = serverResponse[0].id;
+            var liked = serverResponse[0].liked;
+            var total = serverResponse[0].total;
+            liked++;
+            total++;
+            var newData = {
+                "id": id,
+                "liked": liked,
+                "total": total
+            };
+            $.post(url, newData);
+        });
+    };
+
+    DataBase.prototype.updateDislike = function (bookId) {
+        var url = this.serverUrl;
+        $.get(url + "?bookId=" + bookId, function (serverResponse) {
+            var id = serverResponse[0].id;
+            var total = serverResponse[0].total;
+            total++;
+            var newData = {
+                "id": id,
+                "total": total
+            };
+            $.post(url, newData);
+        });
+    };
+
+    App.DataBase = DataBase;
+    window.App = App;
+})(window);

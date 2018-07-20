caed0904fefef00da9ffe778f8391a668236ea0e
himanshup <himanshup@csu.fullerton.edu>
2018-07-20 01:33:29 -0700

    updated all files

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,133 @@
+$.fn.extend({
+	animateCss: function (animationName, callback) {
+		var animationEnd = (function (el) {
+			var animations = {
+				animation: 'animationend',
+				OAnimation: 'oAnimationEnd',
+				MozAnimation: 'mozAnimationEnd',
+				WebkitAnimation: 'webkitAnimationEnd',
+			};
+
+			for (var t in animations) {
+				if (el.style[t] !== undefined) {
+					return animations[t];
+				}
+			}
+		})(document.createElement('div'));
+
+		this.addClass('animated ' + animationName).one(animationEnd, function () {
+			$(this).removeClass('animated ' + animationName);
+
+			if (typeof callback === 'function') callback();
+		});
+
+		return this;
+	},
+});
+
+function animations() {
+	// animateCss function does the animation one time and then something after
+	// this is all the animations you see in the beginning
+	$('#tinder').animateCss('fadeIn');
+	$('#likeBtn').animateCss('bounceInUp');
+	$('#dislikeBtn').animateCss('bounceInUp');
+	$('.alert-secondary').animateCss('fadeInUpBig');
+}
+
+var counter = 0;
+
 (function (window) {
-    "use strict";
-    var $ = window.jQuery;
-    var SERVER_URL = "http://localhost/books";
-    var App = window.App;
-    var DataBase = App.DataBase;
-    var remoteDB = new DataBase(SERVER_URL);
+	var $ = window.jQuery;
+	var SERVER_URL = 'http://localhost:2403/books';
+	var App = window.App;
+	var DataBase = App.DataBase;
+	var remoteDB = new DataBase(SERVER_URL);
 
-    $.getJSON("example.json").done(function (result) {
-        var numberOfBooks = result.length;
-        remoteDB.initialize(numberOfBooks); //Use this to initialize the database
-        for (var i = numberOfBooks - 1; i < numberOfBooks; i--) {
-            var book = result[i];
-            GenerateHTML(book);
-        }
-    });
+	generateBooks();
+	animations();
+	checkClick();
 
-    function GenerateHTML(book) {
-        var $div = $('<div class="tinder-card"></div>');
-        var $text = '<div class="card" style="width: 20rem; height: 36rem;"><img class="card-img-top" src="/' + book.imagePath + '">';
-        $text += '<div class="card-body">' + '<h5 class="card-title">' + book.title + "</h5>";
-        // $text += '<p class="card-text">' + book.summary + "</p>";
-        $text += '<h6 class="card-subtitle mb-2 text-muted">' + book.genre + '</h6>' + '</div></div>';
-        $div.append($text);
-        $("#tinder").append($div);
-    }
+	function generateBooks() {
+		$.getJSON('example.json').done(function (result) {
+			var numberOfBooks = result.length;
+			remoteDB.initialize(numberOfBooks).then(function () {
+				remoteDB.getAll().then(function (databaseBooks) {
+					for (var i = numberOfBooks - 1; i < numberOfBooks; i--) {
+						var book = result[i];
+						generateHTML(book);
+					}
+				});
+			});
+		});
+	}
 
-    document.getElementById("dislikeBtn").addEventListener("click", function () {
-        remoteDB.updateDislike($("#book").val());
-    });
-    document.getElementById("likeBtn").addEventListener("click", function () {
-        remoteDB.updateLiked($("#book").val());
-    });
-    
+	function generateHTML(book, bookDB) {
+		var $div = $('<div class="tinder-card" id="book' + book.id + '"></div>');
+		var $text = '<div class="card" style="width: 20rem; height: 37rem;"><img class="card-img-top" src="' + book.imagePath + '">';
+		$text += '<div class="card-body">' + '<h5 class="card-title">' + book.title + "</h5>";
+		// $text += '<p class="card-text">' + book.author + "</p>";
+		$text += '<h6 class="card-subtitle mb-2 text-muted">' + book.genre + '</h6>' + '</div></div>';
+		$div.append($text);
+		$("#tinder").append($div);
+	}
+
+	function checkNumOfBooks() {
+		if (document.getElementsByClassName('tinder-card').length === 0) {
+			document.getElementById('no-cards').style.visibility = "visible";
+			$('.tinder-buttons').animateCss('fadeOut', function () {
+				document.querySelector('.tinder-buttons').style.visibility = "hidden";
+			});
+			$('.alert').animateCss('fadeOut', function () {
+				document.querySelector('.alert').style.visibility = "hidden";
+			});
+			document.getElementById('reset-button').addEventListener('click', function () {
+				window.location.reload();
+			});
+		}
+	}
+
+	function updateTinder() {
+		document.getElementById('book' + counter).remove();
+		counter++;
+		// remoteDB.updateLiked($('#book').val());
+		$('#book').val(function (i, oldval) {
+			return ++oldval;
+		});
+		checkNumOfBooks();
+	}
+
+	function checkClick() {
+		document.getElementById('likeBtn').addEventListener('click', function () {
+			if (document.getElementsByClassName('tinder-card').length === 2) {
+				$('#' + 'book' + counter).animateCss('rotateOutUpRight', function () {
+					updateTinder();
+				});
+				$('#tinder-shadow').animateCss('rotateOutUpRight', function () {
+					document.getElementById('tinder-shadow').remove();
+					checkNumOfBooks();
+				});
+			} else {
+				$('#' + 'book' + counter).animateCss('rotateOutUpRight', function () {
+					updateTinder();
+				});
+			}
+		});
+
+		document.getElementById('dislikeBtn').addEventListener('click', function () {
+			if (document.getElementsByClassName('tinder-card').length === 2) {
+				$('#' + 'book' + counter).animateCss('rotateOutUpLeft', function () {
+					updateTinder();
+				});
+				$('#tinder-shadow').animateCss('rotateOutUpLeft', function () {
+					document.getElementById('tinder-shadow').remove();
+					checkNumOfBooks();
+				});
+			} else {
+				$('#' + 'book' + counter).animateCss('rotateOutUpLeft', function () {
+					updateTinder();
+				});
+			}
+		});
+	}
+
 })(window);

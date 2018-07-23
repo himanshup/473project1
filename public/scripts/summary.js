0ca2f4813224693750fcf096a6fbaa342720498e
himanshup <himanshup@csu.fullerton.edu>
2018-07-23 05:52:17 -0700

    added percentage for each book

--- public/scripts/summary.js
+++ public/scripts/summary.js
@@ -39,22 +39,30 @@ var counter = 0;
 	checkClick();
 
 	function generateBooks() {
-		$.getJSON('books.json').done(function (result) {
+		$.getJSON("books.json").done(function (result) {
 			var numberOfBooks = result.length;
+			//var percentage=0;
+
 			remoteDB.initialize(numberOfBooks).then(function () {
 				remoteDB.getAll().then(function (databaseBooks) {
-					for (var i = numberOfBooks - 1; i < numberOfBooks; i--) {
+					for (var i = 0; i < numberOfBooks; i++) {
 						var book = result[i];
-						generateHTML(book);
+						var bookDB = databaseBooks[i];
+						if (bookDB["total"] === 0) {
+							percentage = 0;
+						} else {
+							percentage = Math.floor((bookDB["liked"] / bookDB["total"]) * 100)
+						}
+						generateHTML(book, percentage);
 					}
 				});
 			});
 		});
 	}
 
-	function generateHTML(book) {
+	function generateHTML(book, percentage) {
 		var $div = $('<div class="col-12 col-sm-6 col-md-4 col-lg-4 d-flex align-items-stretch"></div>');
-    var $text = '<div class="card w-100 shadow"><img class="card-img-top example-card" src="' + book.imagePath + '"><div class="card-body"><h5 class="card-title bookTitle">' + book.title + '</h5><h6 class="card-subtitle mb-2 text-muted bookGenre">' + book.genre + '</h6></div><div class="card-footer"><small class="text-muted">80% liked this book</small></div></div></div>'
+		var $text = '<div class="card w-100 shadow"><img class="card-img-top example-card" src="' + book.imagePath + '"><div class="card-body"><h5 class="card-title bookTitle">' + book.title + '</h5><h6 class="card-subtitle mb-2 text-muted bookGenre">' + book.genre + '</h6></div><div class="card-footer"><small class="text-muted">' + percentage + '% liked this book</small></div></div></div>'
 		$div.append($text);
 		$(".row").append($div);
 	}

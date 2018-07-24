8c1f3cb202155b5850901d539a00c5f73cb503e8
himanshup <himanshup@csu.fullerton.edu>
2018-07-24 15:57:12 -0700

    fixed percentage

--- public/scripts/summary.js
+++ public/scripts/summary.js
@@ -17,16 +17,21 @@ var counter = 0;
 					for (var i = 0; i < numberOfBooks; i++) {
 						var book = result[i];
 						var bookDB = databaseBooks[i];
-						generateHTML(book, bookDB);
+						if (bookDB["total"] === 0) {
+							percentage = 0;
+						} else {
+							percentage = Math.floor((bookDB["liked"] / bookDB["total"]) * 100)
+						}
+						generateHTML(book, percentage);
 					}
 				});
 			});
 		});
 	}
 
-	function generateHTML(book, bookDB) {
+	function generateHTML(book, percentage) {
 		var $div = $('<div id="book' + book.id + '" class="col-12 col-sm-6 col-md-4 col-lg-4 d-flex align-items-stretch"></div>');
-		var $text = '<div class="card w-100 shadow"><img class="card-img-top example-card" src="' + book.imagePath + '"><div class="card-body"><h5 class="card-title bookTitle">' + book.title + '</h5><h6 class="card-subtitle mb-2 text-muted bookGenre">' + book.genre + '</h6></div><div class="card-footer"><small class="text-muted">' + Math.floor((bookDB.liked / bookDB.total) * 100) + '% liked this book</small></div></div></div>'
+		var $text = '<div class="card w-100 shadow"><img class="card-img-top example-card" src="' + book.imagePath + '"><div class="card-body"><h5 class="card-title bookTitle">' + book.title + '</h5><h6 class="card-subtitle mb-2 text-muted bookGenre">' + book.genre + '</h6></div><div class="card-footer"><small class="text-muted">' + percentage + '% liked this book</small></div></div></div>'
 		$div.append($text);
 		$(".row").append($div);
 	}

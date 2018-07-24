fda95602d9bbc439dd4e3effeec337fe09f9fffd
himanshup <himanshup@csu.fullerton.edu>
2018-07-23 19:49:29 -0700

    removed animateCss function

--- public/scripts/summary.js
+++ public/scripts/summary.js
@@ -1,31 +1,3 @@
-// animateCss function does the animation one time and then something after
-$.fn.extend({
-	animateCss: function (animationName, callback) {
-		var animationEnd = (function (el) {
-			var animations = {
-				animation: 'animationend',
-				OAnimation: 'oAnimationEnd',
-				MozAnimation: 'mozAnimationEnd',
-				WebkitAnimation: 'webkitAnimationEnd',
-			};
-
-			for (var t in animations) {
-				if (el.style[t] !== undefined) {
-					return animations[t];
-				}
-			}
-		})(document.createElement('div'));
-
-		this.addClass('animated ' + animationName).one(animationEnd, function () {
-			$(this).removeClass('animated ' + animationName);
-
-			if (typeof callback === 'function') callback();
-		});
-
-		return this;
-	},
-});
-
 var counter = 0;
 
 (function (window) {
@@ -40,8 +12,6 @@ var counter = 0;
 	function generateBooks() {
 		$.getJSON("books.json").done(function (result) {
 			var numberOfBooks = result.length;
-			//var percentage=0;
-
 			remoteDB.initialize(numberOfBooks).then(function () {
 				remoteDB.getAll().then(function (databaseBooks) {
 					for (var i = 0; i < numberOfBooks; i++) {
